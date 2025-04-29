84e6ff802be0779d832d4b5aaf9ba95e346e3dc7
dbindung <dbindung185@gmail.com>
2025-04-29 21:38:48 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Auto
+{
+    public sealed void Display()
+    {
+        Console.WriteLine("Auto");
+    }
+}
+class Truck : Auto
+{
+ public void DisplayInfo()
+ {
+ base.Display();
+ }
+}
+class Program
+{
+ static void Main(string[] args)
+ {
+ Truck truck = new Truck();
+ truck.DisplayInfo();
+ Console.ReadKey();
+ }
+}
